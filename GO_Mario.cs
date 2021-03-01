709840c29937aa49bb29b07da2d1e56ecc1fccd7
Minoord <70703952+Minoord@users.noreply.github.com>
2021-03-01 11:51:35 +0100

    Game_Object(Player) toegevoegd

---
+++ GO_Mario.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GO_Mario : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public float speed = 4.0f;
+	
+	void Update()
+	{
+		Jump();
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+	}
+	
+	void Jump(){
+		if (Input.GetButtonDown("Jump")) 
+		{
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+		}
+		
+    }
+}
