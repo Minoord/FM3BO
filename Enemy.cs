c0748b35acb3590e63613de2aee399ef61185494
Minoord <70703952+Minoord@users.noreply.github.com>
2021-03-09 12:55:35 +0100

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float maxHealth;
+    float curHealth;
+
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
+
+        if (curHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        curHealth -= damage;
+        Debug.Log("Damage");
+    }
+
+} 
